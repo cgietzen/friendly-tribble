86ede73cf9e4561ed4322abe57a0dca08a4fcc18
Christian Gietzen <christian.gietzen@gmail.com>
2023-02-15 00:23:32 -0500

    anim page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            gridTemplateRows: {
+                9: 'repeat(9, minmax(0, 1fr))',
+            },
+        },
+    },
+    plugins: [],
 }
