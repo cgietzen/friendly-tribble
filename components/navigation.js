387a3ed3eb7fda652ce76ed2a1415046a3518600
Christian Gietzen <christian.gietzen@gmail.com>
2023-03-02 21:11:49 -0500

    adjust nav size

--- components/navigation.js
+++ components/navigation.js
@@ -7,14 +7,14 @@ import { Link } from '@chakra-ui/react'
 export default function Navigation({ toggle }) {
     return (
         <nav class="px-[5%] col-start-1 col-span-12 lg:col-span-1  gap-y-4 lg:pt-10 lg:sticky lg:top-10 lg:h-auto">
-            <div class="lg:sticky lg:top-10 items-center lg:items-end w-full flex lg:flex-col justify-start">
+            <div class=" items-center lg:items-end w-full flex lg:flex-col justify-start">
                 <Link
                     className="inline-block mr-[2.5%] lg:mr-0 lg:mb-5"
                     as={NextLink}
                     href="/"
                 >
                     <Image
-                        className="w-10 lg:w-16 h-auto"
+                        className="w-8 lg:w-16 h-auto"
                         src={siteLogo}
                         alt=""
                         // width={500} automatically provided
@@ -26,7 +26,7 @@ export default function Navigation({ toggle }) {
 
                 <button onClick={toggle}>
                     <Image
-                        className="w-10 lg:w-16 h-auto"
+                        className="w-8 lg:w-16 h-auto"
                         src={contactLogo}
                         alt=""
                         // width={500} automatically provided
