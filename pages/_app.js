c524ea3130d233f0cb8d6637b32c7b0b834cbb0f
Christian Gietzen <christian.gietzen@gmail.com>
2023-03-31 10:33:54 -0400

    actually adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
     return (
         <ChakraProvider>
             <Component {...pageProps} />
+            <Analytics />
         </ChakraProvider>
     )
 }
